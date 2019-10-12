3b1a34bd9773e5d38a74d1fb28536cfd63921adc
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-10-13 01:55:55 +1000

    Formatting

--- usr/src/sys/kern/zones.c
+++ usr/src/sys/kern/zones.c
@@ -23,34 +23,34 @@ sys_zone_create(struct proc *p, void *v, register_t *retval)
 int
 sys_zone_destroy(struct proc *p, void *v, register_t *retval)
 {
-    printf("%s!\n", __func__);
-    return (0);
+    	printf("___________________%s!\n", __func__);
+    	return (0);
 }
 
 int
 sys_zone_enter(struct proc *p, void *v, register_t *retval)
 {
-    printf("%s!\n", __func__);
-    return (0);
+    	printf("___________________%s!\n", __func__);
+    	return (0);
 }
 
 int
 sys_zone_list(struct proc *p, void *v, register_t *retval)
 {
-    printf("%s!\n", __func__);
-    return (0);
+    	printf("___________________%s!\n", __func__);
+    	return (0);
 }
 
 int
 sys_zone_name(struct proc *p, void *v, register_t *retval)
 {
-    printf("%s!\n", __func__);
-    return (0);
+    	printf("___________________%s!\n", __func__);
+    	return (0);
 }
 
 int
 sys_zone_lookup(struct proc *p, void *v, register_t *retval)
 {
-    printf("%s!\n", __func__);
-    return (0);
+    	printf("___________________%s!\n", __func__);
+    	return (0);
 }
